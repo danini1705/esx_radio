48cd154114d0d6c23cd6a5dee554c985779bd5c2
danini1705 <navarro1705@hotmail.de>
2019-03-03 21:20:43 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,5 @@
 ---	SCRIPT CREATED BY DNP
----	DONT RELEASE THAT SCRIPT
+---	Don't claim it as yours
 
 local Keys = {
   ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
